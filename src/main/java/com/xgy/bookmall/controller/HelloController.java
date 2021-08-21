685fa8e7856677a67160e36c8860ecdd3a83bd10
YWXK <1453018126@qq.com>
2021-08-21 23:27:26 +0800

    Post模板

--- src/main/java/com/xgy/bookmall/controller/HelloController.java
+++ src/main/java/com/xgy/bookmall/controller/HelloController.java
@@ -1,25 +1,27 @@
 package com.xgy.bookmall.controller;
 
 import com.xgy.bookmall.entity.User;
+import com.xgy.bookmall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("/")
     public String index(ModelMap map) {
         return "index";
     }
 
-    // get post方法记得加ResponseBody
-    @RequestMapping("/a")
+    @PostMapping("/bcd")
     @ResponseBody
-    public User duser() {
-        return new User();
+    public User dd(@RequestParam("name") String name, @RequestParam("id") String id) {
+        return new User(1, name, "abc", "1587656");
     }
 
 }
