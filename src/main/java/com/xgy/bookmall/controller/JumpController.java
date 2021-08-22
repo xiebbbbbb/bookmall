0e3c56d91abfa36099888c87f976d9c24ca4422c
xiebbbbbb <44255689+xiebbbbbb@users.noreply.github.com>
2021-08-22 15:45:04 +0800

    解决跳转问题

--- src/main/java/com/xgy/bookmall/controller/JumpController.java
+++ src/main/java/com/xgy/bookmall/controller/JumpController.java
@@ -4,19 +4,21 @@ import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+//@RequestMapping("/jump")    // 所有方法都拥有的前缀
 public class JumpController {
 
-    @RequestMapping("/login2")
+    @RequestMapping("/login")
     public String login() {
         System.out.println("11");
-        return "forward:login";
+        return "login";
     }
 
-    @GetMapping("/regist2")
+    @RequestMapping("regist")
     public String regist() {
 
-        return "forward:regist";
+        return "regist";
     }
 }
