adb795b5b82c356c82309d7a1242d164bdf90cae
xiebbbbbb <44255689+xiebbbbbb@users.noreply.github.com>
2021-08-22 15:04:08 +0800

    bug

--- src/main/java/com/xgy/bookmall/controller/JumpController.java
+++ src/main/java/com/xgy/bookmall/controller/JumpController.java
@@ -1,5 +1,6 @@
 package com.xgy.bookmall.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JumpController {
 
-    @RequestMapping("login2")
+    @RequestMapping("/login2")
     public String login() {
-        return "login";
+        System.out.println("11");
+        return "forward:login";
     }
 
-    @GetMapping("regist2")
+    @GetMapping("/regist2")
     public String regist() {
-        return "regist";
+
+        return "forward:regist";
     }
 }
