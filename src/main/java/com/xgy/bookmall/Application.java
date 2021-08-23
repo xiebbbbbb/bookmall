f8f35a265968e0124be82ba6625447076c7eb298
YWXK <1453018126@qq.com>
2021-08-23 21:25:15 +0800

    删除hard code

--- src/main/java/com/xgy/bookmall/Application.java
+++ src/main/java/com/xgy/bookmall/Application.java
@@ -3,6 +3,7 @@ package com.xgy.bookmall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.redisson.Redisson;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,13 @@ import org.springframework.context.annotation.Bean;
 @MapperScan("com.xgy.bookmall.mapper")  // 扫描mapper
 @SpringBootApplication                  // 什么作用？
 public class Application {
+
+    @Value("${spring.redis.host}")
+    String redisHost;
+
+    @Value("${spring.redis.port}")
+    int redisPort;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -17,7 +25,7 @@ public class Application {
     @Bean
     public Redisson redisson() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(0);
+        config.useSingleServer().setAddress(String.format("redis://%s:%d", redisHost, redisPort)).setDatabase(0);
         return (Redisson) Redisson.create(config);
     }
 }
