9db835d24e07090f07655b85f557570a6fc4cfcb
xiebbbbbb <44255689+xiebbbbbb@users.noreply.github.com>
2021-08-24 10:28:05 +0800

    “我的订单”界面完善

--- src/main/java/com/xgy/bookmall/entity/BookDetail.java
+++ src/main/java/com/xgy/bookmall/entity/BookDetail.java
@@ -10,6 +10,9 @@ public class BookDetail {
     private int oId;
     private int bId;
     private int bNum;
+    private float oTotalPrice;
+    private String oAddress;
+    private String oState;
     private String bName;
     private String bDescription;
     private String bAuthor;
@@ -18,10 +21,14 @@ public class BookDetail {
     private String bPublisher;
     private String bUri;
 
-    public BookDetail(int oId, int bId,int bNum, String bName, String bDescription, String bAuthor, String bTag, int bPrice, String bPublisher, String bUri) {
+    public BookDetail(int oId, int bId,int bNum, float oTotalPrice, String oAddress, String oState, String bName,
+                      String bDescription, String bAuthor, String bTag, int bPrice, String bPublisher, String bUri) {
         this.oId = oId;
         this.bId = bId;
         this.bNum = bNum;
+        this.oTotalPrice = oTotalPrice;
+        this.oAddress = oAddress;
+        this.oState = oState;
         this.bName = bName;
         this.bDescription = bDescription;
         this.bAuthor = bAuthor;
